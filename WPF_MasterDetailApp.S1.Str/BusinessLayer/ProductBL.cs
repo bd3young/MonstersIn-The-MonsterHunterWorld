42eb6eab9827a12042ae32920051d9a6ee54be2e
Beau DeYoung <beaudeyoung@gmail.com>
2019-02-16 10:37:18 -0500

    Add project files.

---
+++ WPF_MasterDetailApp.S1.Str/BusinessLayer/ProductBL.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WPF_MasterDetailApp;
+using WPF_MasterDetailApp.Models;
+
+namespace WPF_MasterDetailApp.BusinessLayer
+{
+    public class ProductBL
+    {
+        #region ENUMS
+
+
+
+        #endregion
+
+        #region FIELDS
+
+        ProductWindowView _productWindowView;
+        ProductWindowPresenter _productWindowPresenter;
+
+        #endregion
+
+        #region PROPERTIES
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+        public ProductBL()
+        {
+            //
+            // instantiate the view model and initialize the data set
+            //
+
+            _productWindowPresenter = new ProductWindowPresenter(GetCompanyData(), GetProductData());
+
+            //
+            // instantiate, set the data context, and show the Main Window
+            //
+            _productWindowView = new ProductWindowView(_productWindowPresenter);
+            _productWindowView.DataContext = _productWindowPresenter;
+            _productWindowView.Show();
+        }
+
+        #endregion
+
+        #region METHODS
+
+        private Company GetCompanyData()
+        {
+            return new Company()
+            {
+                Name = "Monsters in the Monster Hunter World!",
+            };
+        }
+
+        private Product GetProductData()
+        {
+            return
+                new Product()
+                {
+                    Name = "Rathalos",
+                    MonsterType = "Large",
+                    MonsterElement = Product.Element.FIRE,
+                    Weakness = Product.Element.THUNDER,
+                    Size = 73.8,
+                    Generation = 1,
+                    ImageFileName = "Rathalos.png",
+                    Description = "Rathalos are large, bipedal wyverns with a spiny, armored hide covering their body. Their outer shell features much brighter and more vibrant colors than that of their female counterpart, the Rathian. It is primarily bright red, with black markings throughout. Like the Rathian, Rathalos possess a flame sac which is used to produce deadly flaming projectiles from the mouth. The talons upon their feet are highly poisonous and are known to to inflict toxic mortal wounds on larger prey. In addition, their long, thick tail features a heavy spiked club at the end. Rathalos' wing membranes feature ornate patterns which are likely used to attract potential mates.",
+                };
+        }
+
+        #endregion
+
+        #region EVENTS
+
+
+        #endregion
+
+    }
+}
---
+++ WPF_MasterDetailApp.S1.Str/Models/Product.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_MasterDetailApp.Models
+{
+	public class Product
+	{
+		#region ENUMS
+
+		public enum Element
+		{
+			WATER,
+			FIRE,
+			ICE,
+			THUNDER,
+			BLAST,
+			POISON,
+			DRAGON
+		}
+
+		#endregion
+
+		#region FIELDS
+
+		private string _name;
+		private string _monsterType;
+		private Element _weakness;
+		private double _size;
+		private int _generation;
+		private Element _monsterElement;
+		private string _description;
+		private string _imageFileName;
+
+		#endregion
+
+		#region PROPERTIES
+
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+
+		public string MonsterType
+		{
+			get { return _monsterType; }
+			set { _monsterType = value; }
+		}
+
+		public Element Weakness
+		{
+			get { return _weakness; }
+			set{ _weakness = value; }
+		}
+
+		public double Size
+		{
+			get { return _size; }
+			set { _size = value; }
+		}
+
+		public int Generation
+		{
+			get { return _generation; }
+			set { _generation = value; }
+		}
+
+		public Element MonsterElement
+		{
+			get { return _monsterElement; }
+			set { _monsterElement = value; }
+		}
+
+		public string Description
+		{
+			get { return _description; }
+			set { _description = value; }
+		}
+
+		public string ImageFileName
+		{
+			get { return _imageFileName; }
+			set { _imageFileName = value; }
+		}
+
+
+		public string ImageFilePath
+		{
+			get { return @"../Images/" + _imageFileName; }
+		}
+
+		#endregion
+
+		#region CONSTRUCTORS
+
+		public Product()
+		{
+
+		}
+
+		#endregion
+
+		#region METHODS
+
+
+		#endregion
+
+		#region EVENTS
+
+
+
+		#endregion
+	}
+}
